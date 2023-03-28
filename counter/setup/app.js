a13970dacabf902c9c05dccdf3fd828be0c680eb
D-future0 <oka4collins@gmail.com>
2023-03-28 04:27:04 +0200

    complete counter project

---
+++ counter/setup/app.js
@@ -0,0 +1,29 @@
+let digit = document.querySelector(`.digit`);
+const btns = document.querySelectorAll(`.btn`);
+let count = 0
+
+btns.forEach(function (btn){
+     btn.addEventListener(`click`, function pick(event) {
+        const typeOfBtn = event.currentTarget.classList;
+        if (typeOfBtn.contains('decrease')) {
+         count--
+        }
+        if (typeOfBtn.contains('reset')) {
+         count=0
+        }
+        if (typeOfBtn.contains('increase')) {
+         count++
+        }
+        digit.innerHTML= count 
+        if (count>0) {
+         digit.style.color =`blue`
+        }
+        else if (count<0) {
+         digit.style.color =`red`
+        }
+        else{
+         digit.style.color =`black`
+        }
+     })
+    });
+    
